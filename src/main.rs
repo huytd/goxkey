04dc9701d8c52b7da17595fab4da6974bbe19aa9
Huy <kingbazoka@gmail.com>
2023-01-11 14:13:37 -0800

    Rewrite the mac frontend using core-graphics and core-foundation crates

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,93 @@
-mod ffi;
-
 use std::{sync::mpsc::channel, thread};
-use ffi::{register_event_tap, EventType};
-use crate::ffi::{CFRunLoopRun, send_string, send_backspace, KEY_A, KEY_D, KEY_F};
+
+use core_foundation::runloop::{CFRunLoop, kCFRunLoopCommonModes};
+use core_graphics::{event::{EventField, CGEventTap, CGEventTapLocation, CGEventTapPlacement, CGEventTapOptions, CGEventType, KeyCode, CGKeyCode, CGEventFlags, CGEvent}, event_source::{CGEventSource, self, CGEventSourceStateID}};
+
+use crate::keymap::get_printable_char;
+
+mod keymap;
+
+fn send_backspace(count: usize) -> Result<(), ()> {
+    let source = CGEventSource::new(event_source::CGEventSourceStateID::Private)?;
+    let backspace_down = CGEvent::new_keyboard_event(source.clone(), KeyCode::DELETE, true)?;
+    let backspace_up = CGEvent::new_keyboard_event(source.clone(), KeyCode::DELETE, false)?;
+
+    for _ in 0..count {
+        backspace_down.post(CGEventTapLocation::HID);
+        backspace_up.post(CGEventTapLocation::HID);
+    }
+
+    Ok(())
+}
+
+fn send_string(string: &str) -> Result<(), ()> {
+    let source = CGEventSource::new(event_source::CGEventSourceStateID::Private)?;
+    let event = CGEvent::new_keyboard_event(source, 0, true)?;
+    event.set_string(string);
+    event.post(CGEventTapLocation::HID);
+    Ok(())
+}
 
 fn main() {
     let (tx, rx) = channel();
 
     thread::spawn(move || {
-        register_event_tap(&tx);
-        println!("KEL is running...");
-        CFRunLoopRun();
+        let current = CFRunLoop::get_current();
+        if let Ok(event_tap) = CGEventTap::new(
+            CGEventTapLocation::HID,
+            CGEventTapPlacement::HeadInsertEventTap,
+            CGEventTapOptions::Default,
+            vec![CGEventType::KeyDown],
+            |_, _, event| {
+                let source_state_id = event.get_integer_value_field(EventField::EVENT_SOURCE_STATE_ID);
+                if source_state_id == 1 {
+                    let key_code = event.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE) as CGKeyCode;
+                    let has_shift = event.get_flags().contains(CGEventFlags::CGEventFlagShift);
+                    _ = tx.send((key_code, has_shift));
+                }
+                None
+            }) {
+            unsafe {
+                let loop_source = event_tap.mach_port.create_runloop_source(0).expect("Somethings is bad ");
+                current.add_source(&loop_source, kCFRunLoopCommonModes);
+                event_tap.enable();
+                CFRunLoop::run_current();
+            }
+        }
     });
 
-    let mut queue: Vec<u16> = vec![];
-
-    // safety net: terminate the app if more than 100 events sent
-    let mut event_count = 0;
-
-    while event_count < 100 {
-        let event = rx.recv().unwrap();
-        match event.etype {
-            EventType::KeyDown => {
-                event_count += 1;
-
-                if event.code == 0x31 {
-                    queue.clear();
-                } else if event.code == 0x33 {
-                    queue.pop();
-                } else {
-                    queue.push(event.code);
-
-                    if queue.len() >= 2 {
-                        if &queue[queue.len() - 2..] == &[KEY_A, KEY_A] {
-                            send_backspace();
-                            send_backspace();
-                            send_string("â");
-                        }
-                        if &queue[queue.len() - 2..] == &[KEY_D, KEY_D] {
-                            send_backspace();
-                            send_backspace();
-                            send_string("đ");
-                        }
-                        if &queue[queue.len() - 2..] == &[KEY_A, KEY_F] {
-                            send_backspace();
-                            send_backspace();
-                            send_string("à");
-                        }
+    let mut buf = String::new();
+    let mut current_word = String::new();
+    loop {
+        if let Ok((key_code, has_shift)) = rx.recv() {
+            match key_code {
+                KeyCode::SPACE | KeyCode::RETURN => {
+                    buf.clear();
+                    current_word.clear();
+                }
+                KeyCode::DELETE => {
+                    buf.pop();
+                }
+                c => {
+                    if let Some(chr) = get_printable_char(c) {
+                        let chs = if has_shift { chr.to_uppercase().to_string() } else { chr.to_lowercase().to_string() };
+                        buf.push_str(&chs);
+                    } else {
+                        buf.clear();
                     }
                 }
-                println!("{:?}", queue);
-            },
-            _ => {}
+            }
+            println!("Buffer: {:?} - Last word: {} - {}", buf, current_word, current_word.chars().count());
+            if buf.len() > 0 {
+                if let (true, result) = vi::telex::transform_buffer(&buf.chars().collect::<Vec<char>>()) {
+                    println!("Transformed: {:?}", result);
+                    let del_count = if !current_word.is_empty() { current_word.chars().count() + 1 } else { buf.chars().count() };
+                    _ = send_backspace(del_count);
+                    _ = send_string(&result);
+                    current_word = result.clone();
+                }
+            }
         }
     }
+
 }
