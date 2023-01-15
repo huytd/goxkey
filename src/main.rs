03c12926cb00815ff58cacecba7f485e5ef25cef
Huy Tran <kingbazoka@gmail.com>
2023-01-15 13:38:58 -0800

    Update to vi-rs 0.3.0

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use platform::{
     run_event_listener, send_backspace, send_string, KEY_DELETE, KEY_ENTER, KEY_ESCAPE, KEY_SPACE,
     KEY_TAB, Handle,
 };
-use std::{cmp::Ordering, sync::Mutex};
+use std::sync::Mutex;
 
-static mut TYPING_BUF: Mutex<Vec<char>> = Mutex::new(vec![]);
+static mut TYPING_BUF: Mutex<String> = Mutex::new(String::new());
 
 fn event_handler(handle: Handle, keycode: char, shift: bool) -> bool {
     unsafe {
@@ -21,20 +21,21 @@ fn event_handler(handle: Handle, keycode: char, shift: bool) -> bool {
             }
             c => {
                 typing_buf.push(if shift { c.to_ascii_uppercase() } else { c });
-            }
-        }
 
-        // TELEX for now, checking if the last key is where the vietnamese tone happen
-        if ['a', 'e', 'o', 'd', 's', 't', 'j', 'f', 'x', 'r', 'w'].contains(&keycode) {
-            let ret = vi::telex::transform_buffer(typing_buf.as_slice());
-            if ret.chars().cmp(typing_buf.clone().into_iter()) != Ordering::Equal {
-                debug!("BUF {:?} - RET {:?}", typing_buf, ret);
-                let backspace_count = typing_buf.len() - 1;
-                debug!("  DEL {} - SEND {}", backspace_count, ret);
-                _ = send_backspace(handle, backspace_count);
-                _ = send_string(handle, &ret);
-                *typing_buf = ret.chars().collect();
-                return true;
+                // TELEX for now, checking if the last key is where the vietnamese tone happen
+                if ['a', 'e', 'o', 'd', 's', 't', 'j', 'f', 'x', 'r', 'w'].contains(&keycode) {
+                    let mut output = String::new();
+                    vi::telex::transform_buffer(typing_buf.chars(), &mut output);
+                    if !typing_buf.eq(&output) {
+                        debug!("BUF {:?} - RET {:?}", typing_buf, output);
+                        let backspace_count = typing_buf.chars().count() - 1;
+                        debug!("  DEL {} - SEND {}", backspace_count, output);
+                        _ = send_backspace(handle, backspace_count);
+                        _ = send_string(handle, &output);
+                        *typing_buf = output;
+                        return true;
+                    }
+                }
             }
         }
     }
