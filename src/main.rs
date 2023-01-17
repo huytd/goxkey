0674d80fb09c713ef0cbacc81602f68b473fccd4
Huy Tran <kingbazoka@gmail.com>
2023-01-17 00:39:50 -0800

    First implementation for the UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod input;
 mod platform;
 
+use druid::{Widget, widget::{Label, Button, Flex}, WidgetExt, WindowDesc, AppLauncher, Data, Lens};
 use input::InputState;
 use lazy_static::lazy_static;
 use log::debug;
@@ -8,7 +9,7 @@ use platform::{
     run_event_listener, send_backspace, send_string, Handle, KeyModifier, KEY_DELETE, KEY_ENTER,
     KEY_ESCAPE, KEY_SPACE, KEY_TAB,
 };
-use std::sync::Mutex;
+use std::{sync::Mutex, thread};
 
 lazy_static! {
     static ref INPUT_STATE: Mutex<InputState> = Mutex::new(InputState::new());
@@ -67,7 +68,52 @@ fn event_handler(handle: Handle, keycode: Option<char>, modifiers: KeyModifier) 
     false
 }
 
+#[derive(Clone, Data, Lens)]
+struct GoxData {
+    input_status: String
+}
+
+impl GoxData {
+    pub fn new() -> Self {
+        let mut ret = Self {
+            input_status: String::new()
+        };
+        let input_state = INPUT_STATE.lock().unwrap();
+        ret.update(&input_state);
+        ret
+    }
+
+    pub fn update(&mut self, input_state: &InputState) {
+        self.input_status = format!("Gõ tiếng Việt = {}", if input_state.enabled { "ON" } else { "OFF" });
+    }
+
+    pub fn toggle_input(&mut self) {
+        let mut input_state = INPUT_STATE.lock().unwrap();
+        input_state.toggle_vietnamese();
+        self.update(&input_state);
+    }
+}
+
+fn main_ui_builder() -> impl Widget<GoxData> {
+    let label = Label::new(|data: &String, _env: &_| data.clone())
+        .lens(GoxData::input_status)
+        .padding(5.0);
+    let button = Button::new("Chuyển chế độ gõ")
+        .on_click(|_, data: &mut GoxData, _| {
+            data.toggle_input();
+        })
+        .padding(5.0);
+    Flex::column().with_child(label).with_child(button).padding(5.0)
+}
+
 fn main() {
     env_logger::init();
-    run_event_listener(&event_handler);
+    thread::spawn(|| {
+        run_event_listener(&event_handler);
+    });
+
+    let win = WindowDesc::new(main_ui_builder)
+        .title("gõkey")
+        .window_size((300.0, 100.0));
+    _ = AppLauncher::with_window(win).launch(GoxData::new());
 }
